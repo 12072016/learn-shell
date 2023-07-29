98db44e92b8074b348407182565bc5c8d3cb7275
neeli <neeliveti.kirankumar@gmai.com>
2023-07-29 18:44:22 +0530

    echo command color options

--- 02-echo.sh
+++ 02-echo.sh
@@ -1,1 +1,14 @@
 echo Hellow World
+
+
+#to enable colour
+##sysntax -
+# echo -e "\e[COLmMESSAGE\e[0m"
+###-e- this is to enable \e (enable colour)
+#### input should be there in double quotes
+### \e - enable a color
+### COLm -color code   >>>> RED[31m] , GREEN[32m], YELLOW[33m], BLUE[34m], MAGENTA[35m], CYAN[36m]
+### \e[0m - to disable the color. (once we enable the color we have to disable that, else color follows to next
+lines asweel.
+
+examle print it in red color: echo -e "\e[32mRED color\e[0m"
