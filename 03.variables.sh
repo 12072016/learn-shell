7981917c96e27c5b6049a1cd9fd81159e7498d31
neeli <neeliveti.kirankumar@gmai.com>
2023-07-30 17:38:55 +0530

    echo command color options

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+### if u assign to a name to set of of a data called variable:
+### a=10
+#####name=devops
+##### print variable
+ echo a = $a
+ echo name=${name}
+#####  or echo name =${name}
+
+a=10
+name=devops
