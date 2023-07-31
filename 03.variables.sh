55d847687b1c6ee4e74d4397cb7a53b38e0b1464
neeli <neeliveti.kirankumar@gmai.com>
2023-07-31 22:11:11 +0530

    variables

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,6 +1,6 @@
 ### if u assign to a name to set of of a data called variable:
-### a=10
-#####name=devops
+a=10
+name=devops
 ##### print variable
  echo a = $a
  echo name=${name}
@@ -8,3 +8,5 @@
 
 a=10
 name=devops
+date=2023-04-18
+echo today data is ${date}
