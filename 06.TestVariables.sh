1c4ca19df0161f4708357fa9a0753ba67090dc65
neeli <neeliveti.kirankumar@gmai.com>
2023-08-02 22:44:15 +0530

    variable test

--- 06.TestVariables.sh
+++ 06.TestVariables.sh
@@ -11,6 +11,13 @@ echo "${person1}: good morning, ${person2}!!!"
 
 
 ## argumentpASS
+
+person1=$1
+person2=$2
+
+
+
+
 sh 06.testVariables.sh $1 $2 $3
 echo "${person1}: Hi, ${person2}"
 echo "${person2}: Hellow, ${person1}"
