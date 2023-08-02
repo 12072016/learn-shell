ad75dd91923e919190ae8c8695b1cf147fca7a26
neeli <neeliveti.kirankumar@gmai.com>
2023-08-02 22:37:34 +0530

    variable test

--- 06.TestVariables.sh
+++ 06.TestVariables.sh
@@ -2,9 +2,9 @@
 
 #!/bin/bash
 
-person1=kiran
-person2=keerthi
+person1=ramu
+person2=ramesh
 echo "${person1}: Hi, ${person2}"
-echo"${person2}: Hellow, ${person1}"
+echo "${person2}: Hellow, ${person1}"
 echo "${person1}: good morning, ${person2}!!!"
 
