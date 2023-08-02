786bb446984779a8be25e2b9a759682d4da73d8f
neeli <neeliveti.kirankumar@gmai.com>
2023-08-02 22:30:41 +0530

    variable test

--- TestVariables.sh
+++ TestVariables.sh
@@ -1,15 +1,10 @@
-##shebang is a always in first line:
-#!/bin/bash
-echo "Hellow World"
-
-##Variables:
 
 
 #!/bin/bash
 
 person1=kiran
 person2=keerthi
-echo "hellow ${person}"
+
 
 echo "${person1}: Hi, ${person2}"
 echo"${person2}: Hellow, ${person1}"
