3461232607468b46ae25c6c9f7f580705245f188
neeli <neeliveti.kirankumar@gmai.com>
2023-08-03 10:55:15 +0530

    variable date

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,30 +1,14 @@
-### if u assign to a name to set of of a data called variable:
+if we assign to a name for set of data called variable;
 a=10
+
 name=devops
-##### print variable
- echo a = $a
- echo name=${name}
-#####  or echo name =${name}
+#PRint
+echo a =${a}
+echo name =${name}
 
-a=10
-name=devops
-COMMAND SUBSTITUTION:
-#DATE=2023-04-18
-DATE=$(date +%F)
-echo today date is ${DATE}
-ARTHMETIC SUBSTITUTION
 
-#ex
+DATE=2023-08-03
+echo today date is =${DATE}
 
-ARTH=$((2-3*4/2))
-ECHO ARTH=${ARTH}
-
-## special variables for inputs:
-
-echo Script name - $0
-echo First Argument -$1
-echo Second Argument -$2
-echo All Argument -$*
-echo No of  Argument -$#
 
 
