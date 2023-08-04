0414e00db4425491d344f6058e09e2acdbc54c04
neeli <neeliveti.kirankumar@gmai.com>
2023-08-04 21:57:22 +0530

    special variables for inputs

--- 05.if.sh
+++ 05.if.sh
@@ -1,7 +1,10 @@
-person_name=kiran
+#### always doublequotes variables while using in expressions
+person_name=ramesh
 age=30
 
-if  [ $person_name == "kiran" ]
+if  [ "$person_name" == "kiran" ]
 then
   echo kiran age - $age
+  else
+    echo person not matching
   fi
